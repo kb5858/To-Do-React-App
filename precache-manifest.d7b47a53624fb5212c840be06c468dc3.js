c1209d67ac2e8b68059e4a315d2ab21cd72d7bda
kb5858 <bkomal5858@gmail.com>
2020-09-16 07:47:03 +0530

    Updates

---
+++ precache-manifest.d7b47a53624fb5212c840be06c468dc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed7cceb330be7c0f0fa3ad757ec5ab77",
+    "url": "/React-To-Do-app/index.html"
+  },
+  {
+    "revision": "bfd09355114304c4e24f",
+    "url": "/React-To-Do-app/static/css/main.4f265788.chunk.css"
+  },
+  {
+    "revision": "451c65665021a591a282",
+    "url": "/React-To-Do-app/static/js/2.cf2e28a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-To-Do-app/static/js/2.cf2e28a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfd09355114304c4e24f",
+    "url": "/React-To-Do-app/static/js/main.ecefb197.chunk.js"
+  },
+  {
+    "revision": "07bda5b1677aa8c869d7",
+    "url": "/React-To-Do-app/static/js/runtime-main.c68f9336.js"
+  }
+]);
